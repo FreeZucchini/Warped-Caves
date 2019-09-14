bc2c30ec25d31defc4d02cd8f5d0ac8b2cccfa94
FreeZucchini <34876143+FreeZucchini@users.noreply.github.com>
2019-09-14 17:34:28 +1200

    Add files via upload

---
+++ Scripts/EnemyAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float speed = 2f;
+
+    public Transform groundDetector;
+    //public Transform wallDetector;
+
+    public bool facingRight = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if (facingRight == true)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+                facingRight = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        RaycastHit2D ground = Physics2D.Raycast(groundDetector.position, Vector2.down, 2f);
+        if (ground.collider == false)
+        {
+            if (facingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingRight = true;
+            }
+        }
+    }
+}
